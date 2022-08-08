b336fbcd237bf5b0584379b498011ffd3c3cf81e
Liu <1752989@tongji.edu.cn>
2022-08-08 14:58:27 +0800

    IDEA Git Test

---
+++ HellorWorld/src/main/java/com/tongji/liu/MainApplication.java
@@ -0,0 +1,15 @@
+package com.tongji.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liu
+ * @create 2022-07-07 12:12
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
---
+++ HellorWorld/src/main/java/com/tongji/liu/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.tongji.liu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liu
+ * @create 2022-07-07 12:13
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/helloworld")
+    public String helloworld(){
+        return "Hello World!";
+    }
+}
